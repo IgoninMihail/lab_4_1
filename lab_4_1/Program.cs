c72581cab36481abfc32a5e6202a4d4f6da8c15f
IgoninMihail <92269370+IgoninMihail@users.noreply.github.com>
2021-12-24 08:40:10 +0200

    Добавьте файлы проекта.

---
+++ lab_4_1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace lab_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] mas = new int[8];
+            int rows = mas.GetUpperBound(0) + 1;
+            Random rnd = new Random();
+            for (int i = 0; i < 8; i++)
+            {
+                mas[i] = rnd.Next(-10, 0);
+            }
+            for (int i = 0; i < 8 - 1; i++)
+            {
+                for (int j = 0; j < 8 - 1; j++)
+                {
+                    if (mas[j] > mas[j + 1])
+                    {
+                        int tmp = mas[j];
+                        mas[j] = mas[j + 1];
+                        mas[j + 1] = tmp;
+                    }
+                }
+
+
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write($"{mas[i]} \t");
+            }
+
+            int[] mas2 = new int[8];
+            int rows2 = mas.GetUpperBound(0) + 1;
+            Random rnd2 = new Random();
+            for (int i = 0; i < 8; i++)
+            {
+                mas2[i] = rnd2.Next(0, 10);
+            }
+            for (int i = 0; i < 8 - 1; i++)
+            {
+                for (int j = 0; j < 8 - 1; j++)
+                {
+                    if (mas2[j] < mas2[j + 1])
+                    {
+                        int tmp = mas2[j];
+                        mas2[j] = mas2[j + 1];
+                        mas2[j + 1] = tmp;
+                    }
+                }
+
+
+            }
+            for (int i = 0; i < rows2; i++)
+            {
+                Console.Write($"{mas2[i]} \t");
+            }
+
+
+        }
+    
+    }
+}
